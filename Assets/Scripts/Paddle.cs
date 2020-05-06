b294f815ae62094e62fbfc7a656466d4d140dab2
rko1985 <35703264+rko1985@users.noreply.github.com>
2020-05-06 07:36:50 -1000

    limiting paddle position

--- Assets/Scripts/Paddle.cs
+++ Assets/Scripts/Paddle.cs
@@ -3,8 +3,12 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Paddle : MonoBehaviour
+
+    //Configuration parameters
 {
-    [SerializeField] float screenWidthInUnits = 16f;
+    [SerializeField] float minX = 1f;
+    [SerializeField] float maxX = 15f;
+    [SerializeField] float screenWidthInUnits = 16f;    
 
     // Start is called before the first frame update
     void Start()
@@ -16,7 +20,8 @@ public class Paddle : MonoBehaviour
     void Update()
     {
         float mousePostInUnits = Input.mousePosition.x / Screen.width * screenWidthInUnits;
-        Vector2 paddlePos = new Vector2(mousePostInUnits, transform.position.y);
+        Vector2 paddlePos = new Vector2(transform.position.x, transform.position.y);
+        paddlePos.x = Mathf.Clamp(Input.mousePosition.x / Screen.width * screenWidthInUnits, minX, maxX);
         transform.position = paddlePos;
     }
 }
