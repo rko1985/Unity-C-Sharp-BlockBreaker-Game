98a9cdae3a5eb8d5fa010e07320c61c6ddefe4d3
rko1985 <35703264+rko1985@users.noreply.github.com>
2020-05-11 08:21:11 -1000

    add player score

--- Assets/Scripts/GameStatus.cs
+++ Assets/Scripts/GameStatus.cs
@@ -4,16 +4,21 @@ using UnityEngine;
 
 public class GameStatus : MonoBehaviour
 {
+    //Config Params
     [Range(0.1f, 10f)] [SerializeField] float gameSpeed = 1f;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField] int pointsPerBlockDestroyed = 83;
 
+    //State Variables
+    [SerializeField] int currentScore = 0;
+    
     // Update is called once per frame
     void Update()
     {
         Time.timeScale = gameSpeed;
+    }
+
+    public void AddToScore()
+    {
+        currentScore += pointsPerBlockDestroyed;
     }
 }
