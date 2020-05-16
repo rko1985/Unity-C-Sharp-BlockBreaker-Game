288f65df6363be906721568fa48185db32102a99
rko1985 <35703264+rko1985@users.noreply.github.com>
2020-05-16 12:02:33 -1000

    autoplay game for fast testing

--- Assets/Scripts/Paddle.cs
+++ Assets/Scripts/Paddle.cs
@@ -2,26 +2,42 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Paddle : MonoBehaviour
-
-    //Configuration parameters
+public class Paddle : MonoBehaviour    
 {
+    //Configuration parameters
     [SerializeField] float minX = 1f;
     [SerializeField] float maxX = 15f;
-    [SerializeField] float screenWidthInUnits = 16f;    
+    [SerializeField] float screenWidthInUnits = 16f;
+
+    //Cached references
+    GameSession theGameSession;
+    Ball theBall;
+
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        theGameSession = FindObjectOfType<GameSession>();
+        theBall = FindObjectOfType<Ball>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        float mousePostInUnits = Input.mousePosition.x / Screen.width * screenWidthInUnits;
         Vector2 paddlePos = new Vector2(transform.position.x, transform.position.y);
-        paddlePos.x = Mathf.Clamp(Input.mousePosition.x / Screen.width * screenWidthInUnits, minX, maxX);
+        paddlePos.x = Mathf.Clamp(GetXPos(), minX, maxX);
         transform.position = paddlePos;
+    }
+
+    private float GetXPos()
+    {
+        if (theGameSession.IsAutoPlayEnabled())
+        {
+            return theBall.transform.position.x;
+        }
+        else
+        {
+            return Input.mousePosition.x / Screen.width * screenWidthInUnits;
+        }
     }
 }
